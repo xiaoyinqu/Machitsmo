c719af8badfd5108d53df24fcaa72c0a0c9f480c
QuXiaoyin <quxiaoyin@QuXiaoyins-MacBook-Air.local>
2015-01-06 21:33:06 +0800

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by QuXiaoyin on 1/6/15.
+//  Copyright (c) 2015 QuXiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by QuXiaoyin on 1/6/15.
+//  Copyright (c) 2015 QuXiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
